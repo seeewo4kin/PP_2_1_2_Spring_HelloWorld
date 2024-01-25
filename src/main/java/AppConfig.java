7a9e48cf5e94d5fdde81568e42a99b6ed6463bc9
seeewo4kin <sewo4kin321@gmail.com>
2024-01-26 00:55:23 +0300

    2.1.1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,10 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Scope("prototype")
+    @Bean(name="cat")
+    public Cat cat() {
+        Cat cat = new Cat();
+        return cat;
+    }
 }
